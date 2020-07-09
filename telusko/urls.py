201eedafcdda92b1429ee330b22e646dddfdfc00
Rustam-Z <zokirovrustam202@gmail.com>
2020-07-09 17:33:44 +0500

    user registration is not working

--- telusko/urls.py
+++ telusko/urls.py
@@ -18,11 +18,11 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('travello.urls')),
-    path('accounts/', include('accounts.urls')),
 
-]
+urlpatterns = [
+    path('', include('travello.urls')),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+] 
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
